b25f0bd971f71291dcd5d3054b8dea7207f9e983
Dulio Denis <dulio.denis@yahoo.com>
2018-04-21 21:56:07 -0400

    Added a 10 second timer based progress bar

--- GifCharades/GifCharades/Controller/GifController.swift
+++ GifCharades/GifCharades/Controller/GifController.swift
@@ -9,12 +9,58 @@
 import UIKit
 import SDWebImage
 
-
 class GifController: UIViewController {
 
     @IBOutlet weak var gifImageView: UIImageView!
-
+    @IBOutlet weak var progressBar: UIView!
+    var animationTimer : Timer?
+    var currentTime = 0.0
+    var totalTime = 10.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        progressBar.frame.size.width = 0
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        invalidateTimer()
+    }
+    
+    fileprivate func invalidateTimer() {
+        animationTimer?.invalidate()
+        animationTimer = nil
+    }
+    
+    fileprivate func timeUp() {
+        print("TIMES UP")
+    }
+    
+    fileprivate func invokeTimer() {
+        if animationTimer == nil {
+            self.currentTime = 0
+            self.progressBar.frame.size.width = 0
+            
+            animationTimer = Timer.scheduledTimer(withTimeInterval: 0.1,
+                                                  repeats: true,
+                                                  block: { (timer) in
+                                                    self.currentTime += timer.timeInterval
+                                                    
+                                                    if self.currentTime > self.totalTime {
+                                                        self.invalidateTimer()
+                                                        self.timeUp()
+                                                        
+                                                    } else {
+                                                        self.progressBar.frame.size.width = (self.view.frame.size.width/CGFloat(self.totalTime)) * CGFloat(self.currentTime)
+                                                        self.progressBar.setNeedsDisplay()
+                                                    }
+            })
+        }
+    }
+    
     @IBAction func fetchGif(_ sender: Any) {
+        invokeTimer()
         let g = Giphy(apiKey: Giphy.PublicBetaAPIKey)
         
         g.random("dogs", rating: nil) { gif, err in
